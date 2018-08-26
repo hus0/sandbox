2281e874d12cf6083e57844e292674832245126d
Filip Husak <filip.husak@kedros.sk>
2018-08-26 07:02:20 +0200

    Add arguments to echo server

--- proposals/service_poc/srv.py
+++ proposals/service_poc/srv.py
@@ -1,37 +1,47 @@
 #!/usr/bin/python
 
+import sys
 import socket
 
 IP = '0.0.0.0'
 PORT = 5555
 RECV_DATA_LEN = 1024
+BACKLOG = 5
 
-def listen():
+def start_srv(ip, port):
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     conn.bind((IP, PORT))
     print 'Listening on {0}:{1}'.format(IP, PORT)
-    conn.listen(10)
+    conn.listen(BACKLOG)
 
     while True:
-        curr_conn, address = conn.accept()
+        curr_conn, addr = conn.accept()
         while True:
             data = curr_conn.recv(RECV_DATA_LEN)
 
             if data == 'quit\r\n':
-                curr_conn.shutdown(1)
                 curr_conn.close()
                 break
             elif data == 'stop\r\n':
-                curr_conn.shutdown(1)
                 curr_conn.close()
-                exit()
+                sys.exit(0)
             elif data:
                 curr_conn.send(data)
                 print data
 
 if __name__ == "__main__":
     try:
-        listen()
+        try:
+            ip = sys.argv[1]
+            port = int(sys.argv[2])
+        except IndexError:
+            ip = IP
+            port = PORT
+        finally:
+            start_srv(ip, port)
     except KeyboardInterrupt:
-        pass
+        sys.exit(1)
+    except Exception, exc:
+        print "Error occurred: %s" %exc
+        sys.exit(2)
